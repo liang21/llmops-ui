e83ad124773140d5516c6ec341e3d9cee7c031d3
edy <edy@edys-MacBook-Pro.local>
2025-12-08 14:21:16 +0800

    feat: add detailview

---
+++ src/views/sapce/app/DetailView.vue
@@ -0,0 +1,108 @@
+<script setup lang="ts"></script>
+<template>
+  <div class="min-h-screen">
+    <!-- 顶部导航 -->
+    <header class="flex items-center h-[74px] bg-gray-100 border-b border-gray-200 px-4">
+      顶部导航
+    </header>
+    <!-- 底部内容区 -->
+    <div class="flex flex-row h-[calc(100vh-74px)]">
+      <!-- 左侧的编排 -->
+      <div class="w-2/3 bg-gray-50 h-full">
+        <header class="flex items-center h-16 border-b border-gray-200 px-7 text-xl text-gray-700">
+          应用编排
+        </header>
+        <div class="flex flex-row h-[calc(100%-64px)]">
+          <div class="flex-1 border-r border-gray-200 p-6">人设与回复逻辑</div>
+          <div class="flex-1 p-6">应用能力</div>
+        </div>
+      </div>
+      <!-- 右侧调试与预览 -->
+      <div class="flex flex-col w-1/3 bg-white h-full">
+        <!-- 调试与预览 -->
+        <header
+          class="flex flex-shrink-0 items-center h-16 px-4 text-xl bg-white border-b border-gray-200 shadow-sm"
+        >
+          调试与预览
+        </header>
+        <!--调试对话界面-->
+        <div class="h-full min-h-0 px-6 py-7 overflow-x-hidden overflow-y-scroll scrollbar-w-none">
+          <!--人类消息-->
+          <div class="flex flex-col gap-2 mb-6">
+            <!--头像-->
+            <a-avatar
+            :style="{backgroundColor:'#3370ff'}"
+            class="flex-shrink-0"
+            :size="30">
+            幕
+            </a-avatar>
+            <a-avatar
+            :style="{backgroundColor:'#00d0b6'}"
+            class="flex-shrink-0"
+            :size="30">
+            <icon-apps/>
+            </a-avatar>
+            <!-- 实际消息 -->
+            <div class="flex flex-col gap-2">
+              <div class="font-semibold text-gray-700">ChatGPT聊天记录</div>
+              <div
+                class="max-w-max bg-blue-700 text-white border border-blue-800 px-4 py-3 rounded-2xl leading-5"
+              >
+                LLM 即 large language
+                model，即大型语言模型,是一种基于深度学习的自然语言处理模型,具有很高的语言理解和生成能力,能够处理长文本信息。
+              </div>
+            </div>
+          </div>
+        </div>
+        <!--没有数据时-->
+        <div class="mt-[200px] flex flex-col items-center justify-center gap-2">
+          <a-avatar :style="{ backgroundColor: '#00d0b6' }" class="flex-shrink-0" :size="30">
+            <icon-apps/>
+          </a-avatar>
+          <!--实际消息-->
+          <div class=" flex flex-col gap-2">
+            <div class="font-semibold text-gray-700">ChatGPT聊天机器人</div>
+            <div class="max-w-max bg-gray-100 text-gray-100 border border-gray-200 px-4 py-3 rounded-2xl leading-5">
+              <icon-loading/>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!--调试对话输入框-->
+      <div class="w-full flex-shrink-0 flex flex-col">
+        <!--顶部输入框-->
+        <div class="pc-6 flex items-center gap-4">
+          <!-- 清除按钮 -->
+            <a-button class="flex-shrink-0" type="text" shape="circle" @click="clearMessages">
+              <template #icon>
+                <icon-empty size="16" :style="{ color: '#374151' }" />
+              </template>
+            </a-button>
+            <!-- 输入框组件 -->
+            <div
+              class="h-[50px] flex items-center gap-2 px-4 flex-1 border border-gray-200 rounded-full"
+            >
+              <input type="text" class="flex-1 outline-0" v-model="query" @keyup.enter="send" />
+              <a-button type="text" shape="circle">
+                <template #icon>
+                  <icon-plus-circle size="16" :style="{ color: '#374151' }" />
+                </template>
+              </a-button>
+              <a-button type="text" shape="circle" @click="send">
+                <template #icon>
+                  <icon-send size="16" :style="{ color: '#1d4ed8' }" />
+                </template>
+              </a-button>
+            </div>
+          </div>
+          <!-- 底部提示文字 -->
+          <div class="text-center text-gray-500 text-xs py-4">
+            内容由AI生成，无法确保真实准确，仅供参考。
+          </div>
+        </div>
+      </div>
+    </div>
+
+</template>
+
+<style scoped></style>
